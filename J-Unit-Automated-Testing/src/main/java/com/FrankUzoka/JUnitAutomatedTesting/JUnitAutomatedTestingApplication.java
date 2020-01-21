7dacc5052d7c3953a4db64f9e0966b6bb833d445
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-21 17:18:22 -0500

    Addin maven files

---
+++ J-Unit-Automated-Testing/src/main/java/com/FrankUzoka/JUnitAutomatedTesting/JUnitAutomatedTestingApplication.java
@@ -0,0 +1,13 @@
+package com.FrankUzoka.JUnitAutomatedTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitAutomatedTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnitAutomatedTestingApplication.class, args);
+	}
+
+}
