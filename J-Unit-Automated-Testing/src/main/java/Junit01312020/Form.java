d176af34560c3f1138f6b27bbdc99a0035772ad9
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 13:14:06 -0500

    Connection to database established

--- J-Unit-Automated-Testing/src/main/java/Junit01312020/Form.java
+++ J-Unit-Automated-Testing/src/main/java/Junit01312020/Form.java
@@ -1,5 +1,7 @@
 package Junit01312020;
 
+import java.util.Objects;
+
 public class Form {
 
     private int id;
@@ -7,5 +9,48 @@ public class Form {
     private String lastName;
 
 
+    public Form(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Form form = (Form) o;
+        return id == form.id &&
+                firstName.equals(form.firstName) &&
+                lastName.equals(form.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
 }
