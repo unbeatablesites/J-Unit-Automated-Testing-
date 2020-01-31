c9eccd952c2aedfb5519504506d6085a7588aea2
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 13:18:52 -0500

    Connection to database established

--- J-Unit-Automated-Testing/src/main/java/Junit01312020/Form.java
+++ J-Unit-Automated-Testing/src/main/java/Junit01312020/Form.java
@@ -1,9 +1,18 @@
 package Junit01312020;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Form {
-
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String firstName;
     private String lastName;
