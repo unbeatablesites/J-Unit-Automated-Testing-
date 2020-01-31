1c7d58c30885ef7df6f89e201e7ee047326aab08
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-31 13:57:36 -0500

    Connection to database established

---
+++ J-Unit-Automated-Testing/src/test/java/com/FrankUzoka/JUnitAutomatedTesting/FormTest.java
@@ -0,0 +1,34 @@
+package com.FrankUzoka.JUnitAutomatedTesting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FormTest {
+
+    @Test
+    void getId() {
+
+
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void getLastName() {
+    }
+
+    @Test
+    void setLastName() {
+    }
+}
